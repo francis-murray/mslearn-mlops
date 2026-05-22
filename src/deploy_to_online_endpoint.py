cc4a6bb5ba5bbd3865b0fea534939ca6c440c999
Francis Murray <franciswmurray@gmail.com>
2026-05-22 17:32:25 +0200

    fix endpoint name to make it unique

--- src/deploy_to_online_endpoint.py
+++ src/deploy_to_online_endpoint.py
@@ -35,7 +35,8 @@ def ensure_endpoint(ml_client: MLClient, endpoint_name: str) -> ManagedOnlineEnd
         return endpoint
     except Exception:
         unique_suffix = datetime.datetime.now().strftime("%m%d%H%M%f")
-        name = endpoint_name or f"endpoint-{unique_suffix}"
+        # name = endpoint_name or f"endpoint-{unique_suffix}"
+        name = f"{endpoint_name}-{unique_suffix}"
 
         endpoint = ManagedOnlineEndpoint(
             name=name,
